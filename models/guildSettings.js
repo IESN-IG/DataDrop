dae471fec857f9ba5ebaa3a33866305a5182ba3f
HunteRoi <tinael.devresse@hotmail.com>
2020-04-26 20:20:51 +0200

    Allows to track what guild's settings is it and if it's still active
    Add 2 props and an update method which allows chaining

--- models/guildSettings.js
+++ models/guildSettings.js
@@ -3,24 +3,30 @@ const get = require('get-value');
 const has = require('has-value');
 
 class GuildSettings {
+  guildId;
   guildPrefix;
   roleIds = {};
   textChannelIds = {};
   voiceChannelIds = {};
   emotes = {};
+  active;
 
   constructor(
+    guildId,
     guildPrefix = 'iesn!',
     roleIds = {},
     textChannelIds = {},
     voiceChannelIds = {},
-    emotes = {}
+    emotes = {},
+    active = true
   ) {
+    this.guildId = guildId.toString();
     this.guildPrefix = guildPrefix;
     this.roleIds = roleIds;
     this.textChannelIds = textChannelIds;
     this.voiceChannelIds = voiceChannelIds;
     this.emotes = emotes;
+    this.active = active;
   }
 
   getValue(key, options) {
@@ -29,10 +35,20 @@ class GuildSettings {
 
   setValue(key, value) {
     set(this, key, value);
+    return this;
   }
 
   hasValue(key) {
     return has(this, key);
+  }
+
+  update(guildSettings) {
+    const properties = Object.keys(guildSettings);
+    properties.forEach(prop => {
+      const propValue = guildSettings[prop];
+      this.setValue(prop, propValue);
+    });
+    return this;
   }
 }
 
